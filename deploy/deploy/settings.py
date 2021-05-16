8e320e2478a55897df942b65dec09263b5d6b1d4
Ruan Jansen van Vuuren <ruan@inventixx.com>
2021-05-16 14:05:59 +0200

    .

--- deploy/deploy/settings.py
+++ deploy/deploy/settings.py
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'deploy.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -81,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -100,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
