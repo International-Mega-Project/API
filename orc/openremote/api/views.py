229e212f77bbeeeba5933cc46a8527a4615b6bcd
JordiVanheeswyck <Jordi.Vanheeswyck@student.pxl.be>
2021-05-27 13:18:36 +0200

    sampleData

--- orc/openremote/api/views.py
+++ orc/openremote/api/views.py
@@ -9,6 +9,20 @@ from django.views.decorators.csrf import csrf_exempt
 from django.db import IntegrityError
 
 
+sampleData = {
+    "12:00": "100",
+    "13:00": "200",
+    "14:00": "250",
+    "15:00": "225",
+    "16:00": "110",
+    "17:00": "100",
+    "18:00": "80",
+    "19:00": "30",
+    "20:00": "35",
+    "21:00": "20",
+    "22:00": "2",
+}
+
 @csrf_exempt
 def get(request):
     # hourly post's
@@ -18,7 +32,7 @@ def get(request):
     body = json.loads(body_unicode)
     try:
         response = JsonResponse(
-            body, status=200
+            sampleData, status=200
         )
         return response
     except IntegrityError:
